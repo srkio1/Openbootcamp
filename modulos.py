3f8f996a4e30f5ebd9bff471da31207db1201cfc
Carlos Daniel <ingdanielruizsaucedo@gmail.com>
2022-10-18 13:47:15 -0400

    ejercicio 1 trabajando con modulos

---
+++ modulos.py
@@ -0,0 +1,32 @@
+import operaciones as op
+
+
+opcion=1
+opcion=2
+opcion=3
+opcion=4
+
+print("""Calculadora basica 
+      Opcion 1 Suma
+      Opcion 2 Resta
+      Opcion 3 Multiplicacion
+      Opcion 4 dividir
+      opcion 5 salir""")
+int(input("elija un numero de la lista: "))
+
+
+if opcion == 1:
+    print(op.suma(5,5))
+elif opcion == 2:
+    print(op.resta(4,4))
+elif opcion == 3:
+    print(op.multiplicar(4,4))
+elif opcion== 4:
+    print(op.dividir(6,2))
+elif opcion==5:
+    print("Hasta luego")
+else:
+    print("algo salio mal")
+    
+
+
---
+++ operaciones.py
@@ -0,0 +1,16 @@
+#operaciones matematicas
+num1 = 0
+num2= 0
+def suma(num1,num2):
+    return num1 + num2
+
+def resta(num1 , num2):
+    return num1 - num2
+
+def multiplicar(num1,num2):
+    return num1 * num2
+
+def dividir(num1,num2):
+    return num1 / num2
+
+
