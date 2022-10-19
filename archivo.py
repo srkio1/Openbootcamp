606f6c59d10bbec22bb738f49df2f39b1163cce7
Carlos Daniel <ingdanielruizsaucedo@gmail.com>
2022-10-18 23:29:08 -0400

    manejo de archivos

---
+++ archivo.py
@@ -0,0 +1,22 @@
+
+#Abrir y guardar archivos
+lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+archivo = open("archivo.txt", "a", encoding="utf-8") # r para solo lectura , w para escribir en el archivo y a para agregar datos en el archivo
+
+for i in lista:
+    a=str(lista.index(i))
+    archivo.write(a +"\n")
+
+archivo.close()
+
+#Leer un archivo y mostrar linea por linea
+
+
+archivo2 = open("archivo.txt", "r", encoding="utf-8")
+for i in archivo2:
+    print(i)
+archivo2.close()
+
+
+
+    
--- trabajo.py
+++ trabajo.py
@@ -3,18 +3,26 @@ from datetime import datetime
 import time
 
 formato = "%H:%M:%S"
-hora=datetime.now().strftime("%H:%M:%S")
-salida = "19:00:00"
 
-h1=datetime.strptime(hora, formato)
-h2=datetime.strptime(salida, formato)
+
+
+    
     
 
-r = h2 - h1
+
+for i in range(3600):
+    hora=datetime.now().strftime("%H:%M:%S")
+    salida = "20:00:00"
+    h1=datetime.strptime(hora, formato)
+    h2=datetime.strptime(salida, formato)
     
-    
-print(f"Aun faltan {r} para salir del trabajo")
-    
+
+    r = h2 - h1
+    if h1<h2:
+        print(f"Aun falta {r} horas para salir del trabajo")
+    else:
+        print(f"Ya es hora de salir del trabajo{r}")
+    time.sleep(1)
         
         
     
